549193044e070074db2ea1a0f43a37dcefff1b73
Michal Adda <addamichal@gmail.com>
2017-09-03 20:59:14 +0200

    testing paypal integration

---
+++ ToptalShop.Api/ToptalShop.Api/Controllers/PaypalPaymentController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+using System.Web.Http;
+using PayPal.Api;
+
+namespace ToptalShop.Api.Controllers
+{
+    public class PaypalPaymentController : ApiController
+    {
+        public IHttpActionResult Post()
+        {
+            if (!ModelState.IsValid) throw new Exception();
+
+            var apiContext = GetApiContext();
+
+            // Create a new payment object
+            var payment = new Payment
+            {
+                experience_profile_id = "XP-5T6Z-S4T6-J8W8-U26E", // Created in the WebExperienceProfilesController. This one is for DigitalGoods.
+                intent = "sale",
+                payer = new Payer
+                {
+                    payment_method = "paypal"
+                },
+                transactions = new List<Transaction>
+                {
+                    new Transaction
+                    {
+                        description = $"Brewery Tour (Single Payment)",
+                        amount = new Amount
+                        {
+                            currency = "USD",
+                            total = 20m.ToString(), // PayPal expects string amounts, eg. "20.00"
+                        },
+                        item_list = new ItemList()
+                        {
+                            items = new List<Item>()
+                            {
+                                new Item()
+                                {
+                                    description = $"Brewery Tour (Single Payment)",
+                                    currency = "USD",
+                                    quantity = "1",
+                                    price = 20m.ToString(), // PayPal expects string amounts, eg. "20.00"                                        
+                                }
+                            }
+                        }
+                    }
+                },
+                redirect_urls = new RedirectUrls
+                {
+                    return_url = "http://localhost/5261/api/paypalpayment/return",
+                    cancel_url = "http://localhost/5261/api/paypalpayment/cancel"
+                }
+            };
+
+            // Send the payment to PayPal
+            var createdPayment = payment.Create(apiContext);
+            return Created("", createdPayment);
+        }
+
+        private APIContext GetApiContext()
+        {
+            // Authenticate with PayPal
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+            return apiContext;
+        }
+    }
+
+    public class PaypalPaymentExecuteController : ApiController
+    {
+        public IHttpActionResult Post(PaymentDto paymentDto)
+        {
+            var apiContext = GetApiContext();
+
+            var paymentExecution = new PaymentExecution()
+            {
+                payer_id = paymentDto.PayerID
+            };
+
+            var payment = new Payment()
+            {
+                id = paymentDto.PaymentID
+            };
+
+            // Execute the Payment
+            var executedPayment = payment.Execute(apiContext, paymentExecution);
+            return Created("", executedPayment);
+        }
+
+        private APIContext GetApiContext()
+        {
+            // Authenticate with PayPal
+            var config = ConfigManager.Instance.GetProperties();
+            var accessToken = new OAuthTokenCredential(config).GetAccessToken();
+            var apiContext = new APIContext(accessToken);
+            return apiContext;
+        }
+    }
+
+    public class PaymentDto
+    {
+        public string PayerID { get; set; }
+        public string PaymentID { get; set; }
+    }
+}
--- ToptalShop.Web/src/app/app.component.ts
+++ ToptalShop.Web/src/app/app.component.ts
@@ -4,17 +4,48 @@ import * as products from './product-list/actions/products';
 import * as fromRoot from './reducers';
 import * as fromLogin from './login/reducers';
 import { Observable } from 'rxjs/Observable';
+declare var paypal: any;
+
+export const CREATE_PAYMENT_URL  = 'http://localhost:5261/api/paypalpayment';
+export const EXECUTE_PAYMENT_URL = 'http://localhost:5261/api/paypalpaymentexecute';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
   loggedIn$: Observable<boolean>;
 
   constructor(private store: Store<fromRoot.State>) {
     this.loggedIn$ = this.store.select(fromLogin.getLoggedIn);
+  }
+
+  ngOnInit() {
+    paypal.Button.render({
+      env: 'sandbox', // Or 'sandbox'
+      commit: true, // Show a 'Pay Now' button
+        payment: function() {
+          return paypal.request.post(CREATE_PAYMENT_URL).then(function(data) {
+          return data.id;
+        });
+      },
+
+      onAuthorize: function (data) {
+        console.log('here!');
+        return paypal.request({
+          method: 'post',
+          url: EXECUTE_PAYMENT_URL,
+          json: {
+            paymentID: data.paymentID,
+            payerID: data.payerID
+        }
+        }).then(function (response) {
+          console.log(response);
+        });
+      }
+
+    }, '#paypal-button');
   }
 }
